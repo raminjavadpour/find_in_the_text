7bb3f2ca3c7aba4752a650568bb79c41b5a65f05
Ramin javadpour <74461882+raminjavadpour@users.noreply.github.com>
2024-01-20 21:42:47 +0330

    Update num.py
    add encoding

--- num.py
+++ num.py
@@ -2,7 +2,8 @@ import re
 
 def extract_mobile_numbers(input_file_path, output_file_path):
     # Read txt file
-    with open(input_file_path, 'r') as file:
+    
+    with open(input_file_path, 'r', encoding="utf-8") as file:
         content = file.read()
 
     # find mobile num started by +98
@@ -14,4 +15,4 @@ def extract_mobile_numbers(input_file_path, output_file_path):
             output_file.write(mobile_number + '\n')
 
 #input file path and output file path
-extract_mobile_numbers('C:/input.txt', 'C:/output.txt')
+extract_mobile_numbers('F:/input.txt', 'F:/output.txt')
